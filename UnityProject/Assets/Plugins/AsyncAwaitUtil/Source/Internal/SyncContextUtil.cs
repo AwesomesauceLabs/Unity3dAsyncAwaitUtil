3b3e8d16d6129d7651798645f9232890b5262da6
Ben Vandervalk <ben.vvalk@gmail.com>
2019-11-08 11:04:21 -0600

    fix: AsyncAwaitUtil in Editor scripts
    Using @StephenHodgson's solution from
    https://github.com/modesttree/Unity3dAsyncAwaitUtil/issues/9#issuecomment-419455961

--- UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
+++ UnityProject/Assets/Plugins/AsyncAwaitUtil/Source/Internal/SyncContextUtil.cs
@@ -10,9 +10,37 @@ namespace UnityAsyncAwaitUtil
 {
     public static class SyncContextUtil
     {
+#if UNITY_EDITOR
+        private static System.Reflection.MethodInfo executionMethod;
+
+        /// <summary>
+        /// HACK: makes Unity Editor execute continuations in edit mode.
+        /// </summary>
+        private static void ExecuteContinuations()
+        {
+            if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                return;
+            }
+
+            var context = SynchronizationContext.Current;
+
+            if (executionMethod == null)
+            {
+                executionMethod = context.GetType().GetMethod("Exec", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            }
+
+            executionMethod?.Invoke(context, null);
+        }
+
+        [UnityEditor.InitializeOnLoadMethod]
+#endif
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static void Install()
         {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.update += ExecuteContinuations;
+#endif
             UnitySynchronizationContext = SynchronizationContext.Current;
             UnityThreadId = Thread.CurrentThread.ManagedThreadId;
         }
